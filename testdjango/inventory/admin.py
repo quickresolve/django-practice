f737a3a79b35507e8b94ffd211da50cc4491b696
QuickResolve <lauranmontoya@gmail.com>
2016-06-18 16:14:23 -0700

    add ItemAdmin class to admin file and include in site registry

--- testdjango/inventory/admin.py
+++ testdjango/inventory/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import Item
 #import Item model into admin
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'amount']
+
+#update list_display by adding ItemAdmin class to overide default
+
+admin.site.register(Item, ItemAdmin)
 #register Item with admin for CRUD functionality
+#include ItemAdmin in argument
