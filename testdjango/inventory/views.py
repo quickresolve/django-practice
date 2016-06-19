f455c70955079cebaf1134c8755ac89481dfc742
QuickResolve <lauranmontoya@gmail.com>
2016-06-18 17:04:36 -0700

    create routes for index & item_detail in view file

--- testdjango/inventory/views.py
+++ testdjango/inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#route for index page
+def index(request):
+    return HttpResponse('<p>In index view</p>')
+
+
+#route for item_detail page
+def item_detail(request, id):
+    return HttpResponse('<p>In item_detail view with id {0}</p>'.format(id))
